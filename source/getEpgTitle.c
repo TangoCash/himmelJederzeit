965881f64b0e6bfa8e6421cfa11f464fa5c39cd3
TangoCash <eric@loxat.de>
2014-11-28 20:26:46 +0100

    add source

---
+++ source/getEpgTitle.c
@@ -0,0 +1,117 @@
+/*
+ * getEpgTitle.c
+ *
+ *  Created on: 18.07.2013
+ *      Author: erdal
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <malloc.h>
+#define TAG1 "<epgtitle>"
+#define TAG2 "</epgtitle>"
+
+char *FindNext(char *sp, char *ep, char *SearchStr, int MaxLen)
+{
+	char c = *SearchStr;
+	ep -= MaxLen;
+	while (sp < ep)
+	{
+		if (*sp == c)
+		{
+			if (memcmp(sp, SearchStr, MaxLen) == 0)
+				return(sp);
+		}
+		sp++;
+	}
+	return(0);
+}
+
+void ParseString(const char *ip, char *op)
+{
+	while (*ip)
+	{
+		if (*ip == '\\')
+		{
+			ip++;
+			if (*ip == '\\')
+			{
+				*op++ = *ip;
+				ip++;
+			}
+			if (*ip == 'n')
+			{
+				*op++ = 10;
+				ip++;
+			}
+			if (*ip == 'r')
+			{
+				*op++ = 13;
+				ip++;
+			}
+		}
+		else
+			*op++ = *ip++;
+	}
+	*op = 0;
+}
+
+void trim(char str[] )
+{
+    if(!str)
+        return;
+
+    char* ptr = str;
+    int len = strlen(ptr);
+
+    while(len-1 > 0 && isspace(ptr[len-1]))
+        ptr[--len] = 0;
+
+    while(*ptr && isspace(*ptr))
+        ++ptr, --len;
+
+    memmove(str, ptr, len + 1);
+}
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int j = 0;
+
+	int tlen = strlen(TAG1);
+	int tlen2 = strlen(TAG2);
+	for (i = 1; i < argc; i++)
+	{
+		FILE *fp = fopen(argv[i], "r");
+		if (fp == NULL)
+		{
+			printf("cannot open file %s\n", argv[i]);
+			continue;
+		}
+		char st[1000];
+		while (fgets(st, sizeof(st), fp) != NULL)
+		{
+			j++;
+			int len = strlen(st);
+			char *p = FindNext(st, st + len, TAG1, tlen);
+			if (p != NULL)
+			{
+				memmove(p, p + tlen, (st + len) - (p + tlen) + 1);
+				char *p = FindNext(st, st + len, TAG2, tlen2);
+				if (p != NULL)
+				{
+					*p = 0;
+				}
+
+				trim(&st);
+
+				trim(st);
+				printf("%s\n", st);
+			}
+		}
+		fclose(fp);
+	}
+	return 0;
+}
+
---
+++ source/listdir.c
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <errno.h>
+/* "readdir" etc. are defined here. */
+#include <dirent.h>
+/* limits.h defines "PATH_MAX". */
+#include <limits.h>
+
+/* List the files in "dir_name". */
+
+static void list_dir(const char * dir_name,FILE *fp) {
+	DIR * d;
+
+	/* Open the directory specified by "dir_name". */
+
+	d = opendir(dir_name);
+
+	/* Check it was opened. */
+	if (!d) {
+		fprintf(stderr, "Cannot open directory '%s': %s\n", dir_name,
+				strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	while (1) {
+		struct dirent * entry;
+		const char * d_name;
+
+		/* "Readdir" gets subsequent entries from "d". */
+		entry = readdir(d);
+		if (!entry) {
+			/* There are no more entries in this directory, so break
+			 out of the while loop. */
+			break;
+		}
+
+		d_name = entry->d_name;
+		if (entry->d_type & DT_REG) {
+
+			/*int i;
+			 const char * end;
+			 for ( i = strlen(d_name)-3;i < strlen(d_name);i++ )
+			 end += d_name[i];*/
+			const char * resultat = strstr(d_name, ".xml\0");
+
+			if (resultat) {
+				//printf("%s/%s\n", dir_name, d_name);
+				char daten[2048] = "";
+				strcat(daten,dir_name);
+				strcat(daten,"/");
+				strcat(daten,d_name);
+				strcat(daten,"\n");
+//				printf("daten: %s\n",daten);
+				fwrite(daten, strlen(daten), 1, fp);
+
+			}
+			/* Print the name of the file and directory. */
+		}
+		/* See if "entry" is a subdirectory of "d". */
+
+		if (entry->d_type & DT_DIR) {
+
+			/* Check that the directory is not "d" or d's parent. */
+
+			if (strcmp(d_name, "..") != 0 && strcmp(d_name, ".") != 0) {
+				int path_length;
+				char path[PATH_MAX];
+
+				path_length = snprintf(path, PATH_MAX, "%s/%s", dir_name,
+						d_name);
+				//printf("%s\n", path);
+				if (path_length >= PATH_MAX) {
+					fprintf(stderr, "Path length has got too long.\n");
+					exit(EXIT_FAILURE);
+				}
+				/* Recursively call "list_dir" with the new path. */
+				list_dir(path,fp);
+			}
+		}
+	}
+
+	/* After going through all the entries, close the directory. */
+	if (closedir(d)) {
+		fprintf(stderr, "Could not close '%s': %s\n", dir_name,
+				strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	const char * dir;
+
+	char file_name[] = "/hdd/movies/myFile";
+	FILE *fp;
+
+	fp = fopen(file_name, "a+"); // read mode
+
+	if (fp == NULL) {
+		perror("Error while opening the file.\n");
+		exit(EXIT_FAILURE);
+	} else
+		printf("Datei handle geöffnet %s\n", file_name);
+
+
+	if (argc > 1) {
+		dir = argv[1];
+	} else {
+		dir = "/hdd/movies/anytime";
+	}
+	list_dir(dir,fp);
+	fclose(fp);
+	return 0;
+}
